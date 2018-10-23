d66118cc652b37f66dcaa2a97da18788312a2f62
rlavelli <riccardo.lavelli7@gmail.com>
2018-10-23 20:58:07 +0200

    added sql db update
    Raw code. Still need to clean up. Need to schedule.
    
    Issue:
    #1 optimize use of sql db
    #2 schedule code

--- webScrape_1.py
+++ webScrape_1.py
@@ -40,4 +40,97 @@ tag_dict
 # trasform in DataFrame
 import pandas as pd
 tag_df = pd.DataFrame.from_dict(tag_dict, orient = "index")
+tag_df.reset_index(level=0, inplace=True)
+tag_df.columns = ["tag", "count"]
+
+df_total = pd.DataFrame(columns=["tag", "count"])
+
+df_total = df_total.append(tag_df)
+
+
+# db save
+import sqlite3
+import os
+
+path = "/Users/Richi/PythonProjects/sql_tutorial/"
+os.chdir(path)
+
+conn = sqlite3.connect("stack_scrape.db") # creates db if not exist
+cur = conn.cursor()
+
+def create_table_tags():
+    cur.execute('CREATE TABLE IF NOT EXISTS tags(tag_name TEXT, count INTEGER)')
+    
+create_table_tags()
+
+def insert_scraped(df_scrape):
+    
+    for index, row in df_scrape.iterrows():
+        tag_scraped = row['tag']
+        count_scraped = row['count']
+        
+        cur.execute('INSERT INTO tags (tag_name, count) VALUES (?,?)',
+                    (tag_scraped, count_scraped))
+        conn.commit()
+        
+insert_scraped(tag_df)
+cur.close()
+conn.close()
+
+# saving output
+fname = "~/PythonProjects/WebCrawler/stack_tags.csv"
+
+df_update = pd.read_csv(fname, index_col = False)
+
+df_update = df_update.append(df_total) # update
+
+df_update.to_csv("~/PythonProjects/WebCrawler/stack_tags.csv", index = False)
+
+# check file
+
+
 # tag_df.head
+
+# --- schedule scraping
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+import schedule
+import time
+
+def job():
+    print("I'm working...")
+    page = requests.get("https://stackoverflow.com/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    p = soup.find_all('a', class_='post-tag')
+    tag_list = []
+    for element in p:
+        tag = element.get_text() # estrae il testo contenuto nei tag selezionati
+        tag_list.append(tag)
+        # count single tags
+    tag_dict = {}
+    for row in tag_list:
+        if (row in tag_dict):
+            tag_dict[row] += 1
+        else:
+            tag_dict[row] = 1
+    tag_df = pd.DataFrame.from_dict(tag_dict, orient = "index")
+    tag_df.reset_index(level=0, inplace=True)
+    tag_df.columns = ["tag", "count"]
+    # append to total
+    df_total = pd.DataFrame(columns=["tag", "count"])
+    df_total = df_total.append(tag_df)
+    # saving output
+    fname = "~/PythonProjects/WebCrawler/stack_tags.csv"
+    df_update = pd.read_csv(fname, index_col = False)
+    df_update = df_update.append(df_total) # update
+    df_update.to_csv("~/PythonProjects/WebCrawler/stack_tags.csv", index = False)
+    
+schedule.every(1).minute.do(job)
+
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
