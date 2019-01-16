2e3a09c9b1beaae9ecfc1130b0599c59bd1e12e1
rlavelli <riccardo.lavelli7@gmail.com>
2019-01-16 15:23:47 +0100

    Update basics.py

--- basics.py
+++ basics.py
@@ -13,3 +13,27 @@ titanic.age = by_sex_class['age'].transform(impute_median) #impute median by gro
 # Print the output of titanic.tail(10)
 print(titanic.tail(10))
 
+
+# corrplot function custom
+import numpy as np
+import seaborn as sns
+
+def plot_correlation_heatmap(df):
+    corr = df.corr()
+    
+    sns.set(style="white")
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    f, ax = plt.subplots(figsize=(11, 9))
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+
+    sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    plt.show()
+
+columns = []
+
+plot_correlation_heatmap(train[columns])
+
