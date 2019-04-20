a8fcea0d5712f0b29b45f9890e9496b8641a1869
rlavelli <riccardo.lavelli7@gmail.com>
2019-04-20 10:52:05 +0200

    Update basics.py

--- basics.py
+++ basics.py
@@ -39,3 +39,34 @@ columns = []
 plot_correlation_heatmap(train[columns])
 
 # ------------------------------------------------------------------------------------------------------------------------------
+
+# Function for plot learning curves for ML models
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=10)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+    plt.legend(loc="upper right", fontsize=14)   # not shown in the book
+    plt.xlabel("Training set size", fontsize=14) # not shown
+    plt.ylabel("RMSE", fontsize=14)              # not shown
+    
+
+from sklearn.pipeline import Pipeline # pipe multiple transformations before model fit
+
+polynomial_regression = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+        ("lin_reg", LinearRegression()),
+    ])
+
+plot_learning_curves(polynomial_regression, X, y)
